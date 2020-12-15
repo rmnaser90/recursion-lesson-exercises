4c62a63ef1615e902b73bb77a184eca3ef3d4a42
rmnaser90 <rmnaser90@gmail.com>
2020-12-15 14:09:59 +0200

    exercises done

--- src/app.js
+++ src/app.js
@@ -4,25 +4,42 @@
 */
 
 //Exercise 1
-const findFactorial = function() {
-  //Your code here
+const findFactorial = function (num, currentTotal) {
+  currentTotal = currentTotal || 1
+  currentTotal *= num
+  num--
+  if (num == 0) { return currentTotal }
+  return findFactorial(num, currentTotal)
 
 }
 
 //Exercise 2
-const reverseString = function() {
-  //Your code here
-
+const reverseString = function (str, reverseStr) {
+  reverseStr = reverseStr || ""
+  if (str.length == 0) {
+    return reverseStr
+  }
+  const arr = str.split('')
+  reverseStr += arr.splice(arr.length - 1, 1)
+  return reverseString(arr.join(''), reverseStr)
 }
+
 
 //Exercise 3
 const arr1 = [1, 2, 3]
 const arr2 = []
-
-const swap = function() {
+const swap = function (arr1,arr2) {
   //Your code here
+ if (arr1.length == 0) {
+   return
+ }
+ arr2.push(arr1.splice(0,1)[0])
 
+swap(arr1,arr2)
 }
 
 /* DO NOT REMOVE THE EXPORTS BELOW */
 module.exports = { findFactorial, reverseString, swap }
+
+
+
